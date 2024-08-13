e249aa9c36f1e38f65262e3609f4ea797c3662e6
Rafael <rafalive122@hotmail.com>
2024-08-13 00:12:33 -0300

    Testes (Estruturando a página)

--- paginaDioBank/my-app-ts/src/App.tsx
+++ paginaDioBank/my-app-ts/src/App.tsx
@@ -1,34 +1,67 @@
 
 import { Layout } from './components/Layout'
 import styled from 'styled-components'
-import { ChakraProvider } from '@chakra-ui/react'
+import { 
+  Center,
+  ChakraProvider,
+  Input,
+  Box,
+  Button 
 
-const Box = styled.div`
-  background-color: orange;
-  border-radius: 25px;
-  padding-left: 5px;
-`
+} from '@chakra-ui/react'
+import { PasswordInput } from './components/Password'
+
+
+// const Box = styled.div`
+//   background-color: orange;
+//   border-radius: 25px;
+//   padding-left: 5px;
+// `
 
 function App() {
   return (
     <ChakraProvider>
-      <Layout>
-        <Box>
-          <h1>Faça Login</h1>
+      <Box minHeight='100vh' backgroundColor='#9413dc'>
+        <Center>
+          <h1>Faça o login</h1>
+        
+
+        <Layout>
+          {/* <Box>
+            <h1>Faça Login</h1>
+          </Box> */}
+          <Input placeholder="email" />
+        
+          {/* <label htmlFor='passwordInput'>
+            Senha
+          </label> */}
+          {/* <input id='passwordInput' type='password'/> */}
+        
+          <PasswordInput></PasswordInput>
+        
+          <button>
+            Entrar
+          </button>
+        </Layout>
+      </Center> 
+      </Box>
+      <Center>
+        <h1>Outro Box</h1>
+        <Box minHeight='100vh' backgroundColor='#9413dc' padding='25px'>
+          <Box backgroundColor= '#FFFFFF' borderRadius='25px' padding='15px'>
+            <Center>
+              <h1>Faça Login</h1>
+            </Center> 
+            <Input placeholder='email'></Input>
+            <Input placeholder='password'></Input>
+            <Button colorScheme='teal' size='sm'>
+              Button
+            </Button>
+          </Box>
         </Box>
+      </Center>
       
-        <label htmlFor='emailInput'>
-          Email
-        </label>
-        <input id='emailInput' type='email'/>
-        <label htmlFor='passwordInput'>
-          Senha
-        </label>
-        <input id='passwordInput' type='password'/>
-        <button>
-          Entrar
-        </button>
-      </Layout>
+
     </ChakraProvider>
   );
 }
